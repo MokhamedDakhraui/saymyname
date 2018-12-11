45cd716f8d66af10d0e2edaf0d4c8b48df145173
lazyfellow <mokhamed_93@mail.ru>
2018-12-12 01:10:05 +0300

    Added project with basic functionality

---
+++ SayMyName/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SayMyName.Models;
+using System.Diagnostics;
+
+namespace SayMyName.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Say()
+        {
+            ViewData["Name"] = Request.Cookies.TryGetValue("name", out string name) ? name : "no name";
+
+            return View();
+        }
+
+        public IActionResult Set()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Set(string name)
+        {
+            Response.Cookies.Append("name", name);
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
